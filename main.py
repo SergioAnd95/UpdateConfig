52a5146b1beac090f88bae100f7fba7547a62110
SergioAnd95 <developinc@yadnex.ru>
2017-06-15 14:16:50 +0300

    update method 'update'

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from pprint import pprint
+from copy import deepcopy
 
 
 def update(data, service, count):
+    """
+    Function for update configuration of service
+    :param data: dict
+    :param service: str
+    :param count: int
+    :return: dict
+    """
+    data_copy = deepcopy(data)
 
-    pass
+    for i in range(count):
+        server_pairs = [(sum(data_copy[d].values()), d) for d in data_copy]
+        min_server = min(server_pairs)[1]
+        data_copy[min_server][service] = data_copy[min_server].get(service, 0) + 1
+    return data_copy
 
 
 def main():
@@ -20,10 +33,10 @@ def main():
     print("Configuration before:")
     pprint(example_data)
 
-    update(example_data, 'pylons', 7)
+    updated_data = update(example_data, 'pylons', 7)
 
     print("Configuration after:")
-    pprint(example_data)
+    pprint(updated_data)
 
 if __name__ == '__main__':
     main()
